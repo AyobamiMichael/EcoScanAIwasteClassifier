b9b8576aad2b6d006eeab9f3821283da7763bf8e
Ayobami <ayobamiwealth@gmail.com>
2025-10-16 21:45:13 +0100

    Update app.py shareable to True

--- app.py
+++ app.py
@@ -521,7 +521,7 @@ if __name__ == "__main__":
     demo.launch(
         server_name="0.0.0.0",
         server_port=7860,
-        share=False,
+        share=True,
         show_error=True,
         debug=True
     )
